70b5dd3fe5ab7bbbb714dbc13029a6fb56bc1a82
hrishita <hrishitajoshi86@gmail.com>
2026-01-01 22:09:36 +0530

    implemented windowcontrol

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/Welcome.jsx";
-import Dock from "#components/Dock.jsx"
-export{ Navbar,Welcome ,Dock };
+import Dock from "#components/Dock.jsx";
+import WindowControls from "#components/WindowControls.jsx";
+
+export { Navbar, Welcome, Dock, WindowControls };
